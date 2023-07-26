8c4241f3497596331cc921fa8f368c01ddd6e2e2
kjungw1025 <kjungw1025@naver.com>
2023-07-26 21:48:21 +0900

    feat: Connect the settings of routes to app

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,9 @@ import MainView from '../views/MainView.vue'
 import CustomView from '../views/CustomView.vue'
 import GymwearView from '../views/GymwearView.vue'
 import CommunityView from '../views/CommunityView.vue'
+import MachineView from '../views/MachineView.vue'
+import MachinepartView from '../views/MachinepartView.vue'
+import WorkoutwayView from '../views/WorkoutwayView.vue'
 
 const routes = [
   {
@@ -26,13 +29,28 @@ const routes = [
     component: CommunityView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/machine',
+    name: 'Machine',
+    component: MachineView
+  },
+  {
+    path: '/machinepart',
+    name: 'MachinePart',
+    component: MachinepartView,
+  },
+  {
+    path: '/workoutway',
+    name: 'Workoutway',
+    component: WorkoutwayView,
+  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
