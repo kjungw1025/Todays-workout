c7ade95c680f13c185150f21ea89817f20c5da39
kjungw1025 <kjungw1025@naver.com>
2023-07-26 20:59:53 +0900

    feat: Update CommunityView.vue

--- frontend/src/views/CommunityView.vue
+++ frontend/src/views/CommunityView.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an community page</h1>
+    <!-- <h1>This is an community page</h1> -->
+    <iframe
+      id="iframe1" 
+      width="100%"
+      height="3000px"
+      src="http://localhost:8003/" 
+      frameborder="0" 
+      allow="autoplay; encrypted-media" 
+      allowfullscreen
+      style="overflow-y:hidden">
+    </iframe>
   </div>
 </template>
+
+<style scoped>
+  /* div {
+    overflow-y: auto !important;
+  } */
+  /* 나중에 http://localhost:8003/를 .env에 따로 보관하는 것도 괜찮을듯 */
+  /* iframe {
+    overflow-y: scroll;
+  }
+  iframe::-webkit-scrollbar {
+    display: none;
+  }
+  iframe {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  } */
+</style>
