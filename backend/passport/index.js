c6b5aaeaad0ff0b0dd1bf74a3690be615bf352b3
kjungw1025 <kjungw1025@naver.com>
2023-07-28 20:03:11 +0900

    fix: passport/index.js

--- backend/passport/index.js
+++ backend/passport/index.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const local = require('./localStrategy');
-const kakao = require('./KakaoStrategy');
+const kakao = require('./kakaoStrategy');
 const User = require('../models/user');
 
 module.exports = () => {
